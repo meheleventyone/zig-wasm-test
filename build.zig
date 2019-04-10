2128b33c8676aa997cbcb5798c8a61a544c9e38f
Charles Palmer <charleswcpalmer@gmail.com>
2019-04-10 19:09:04 +0000

    Updating build script for 0.4.0

--- build.zig
+++ build.zig
@@ -1,22 +1,12 @@
 const Builder = @import("std").build.Builder;
 const builtin = @import("builtin");
 
-const wasmLink : []const []const u8 = []const []const u8 {"wasm-ld", "./zig-cache/wasmtest.o", "-o", "./zig-cache/wasmtest.wasm", "-O2", "--no-entry", "--allow-undefined"};
-
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
-    const obj = b.addObject("wasmtest", "src/main.zig");
-    obj.setBuildMode(mode);
-    obj.setTarget(builtin.Arch.wasm32, builtin.Os.freestanding, builtin.Environ.unknown);
-
-    var main_tests = b.addTest("src/main.zig");
-    main_tests.setBuildMode(mode);
-
-    const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&main_tests.step);
-
-    const wasm_link = b.addCommand(".", b.env_map, wasmLink);
-    wasm_link.step.dependOn(&obj.step);
-
-    b.default_step.dependOn(&wasm_link.step);
+    const exe = b.addExecutable("wasmtest.wasm", "src/main.zig");
+    b.setInstallPrefix(b.build_root);
+    exe.setBuildMode(mode);
+    exe.setTarget(builtin.Arch.wasm32, builtin.Os.freestanding, builtin.Abi.none);
+    b.default_step.dependOn(&exe.step);
+    b.installArtifact(exe);
 }
